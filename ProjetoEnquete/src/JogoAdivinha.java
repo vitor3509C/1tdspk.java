556d10c6227cae95f21e0ebec41dc415a5d9c4fe
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-26 12:08:17 -0300

    projeto.adivinha

---
+++ ProjetoEnquete/src/JogoAdivinha.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class JogoAdivinha {
+
+    public static void main(String args[]) {
+
+        Scanner tec = new Scanner(System.in);
+        Adivinha adv = new Adivinha();
+
+        System.out.print("num: ");
+        int palpite = tec.nextInt();
+        char resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+        System.out.print("num: ");
+        palpite = tec.nextInt();
+        resp = adv.chute(palpite);
+        System.out.println(resp);
+
+    }
+}
